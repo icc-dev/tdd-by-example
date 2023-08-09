2caf06881af79d598e8658f477e5fbcc7c455bdc
Ian Cardenas <icardenasc@devicc.com>
2023-08-09 09:21:30 -0400

    test(GreetingTest): init setup method

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,10 +1,15 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
+    @BeforeEach
+    void setUp() {
+
+    }
 
     @Test
     void helloWorld() {
