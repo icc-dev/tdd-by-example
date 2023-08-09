39e96073aabe146df84edd615791bab1b618ca4c
Ian Cardenas <icardenasc@devicc.com>
2023-08-09 09:36:52 -0400

    test(GreetingTest): implement life cycle methods

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,14 +1,20 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
     private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("In before all");
+    }
+
     @BeforeEach
     void setUp() {
+        System.out.println("In before each...");
         greeting = new Greeting();
     }
 
@@ -20,5 +26,15 @@ class GreetingTest {
     @Test
     void helloSomething() {
         System.out.println(greeting.helloSomething("Ian"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("In after all");
     }
 }
